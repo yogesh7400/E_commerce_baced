06898e7a05815c347a6a3ec9838aa2376179caa1
Yogesh.Mishra <Yogesh.Mishra@DESKTOP-BEU4724.swastik.abmindia.com>
2023-11-07 16:46:29 +0530

    this is my second

--- src/main/java/com/abm/controller/ProductController.java
+++ src/main/java/com/abm/controller/ProductController.java
@@ -3,6 +3,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,6 +21,7 @@ import com.abm.service.ProductService;
 
 @RestController
 @RequestMapping("/api/products")
+@CrossOrigin
 public class ProductController {
 
     @Autowired
